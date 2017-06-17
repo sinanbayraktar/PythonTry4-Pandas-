598254ea4a8ee14e0242b4b31c42872046ee4cd6
Sinan İbrahim Bayraktar <_sinan_@windowslive.com>
2017-06-18 00:55:56 +0300

    Add files via upload

---
+++ numpyTry4(numpy).py
@@ -0,0 +1,17 @@
+import numpy
+array1 = numpy.array([1, 2, 3])
+array2 = numpy.array([4, 5, 6])
+sum = array1 + array2
+mult = array1 * array2
+print (sum)
+print (mult)
+array3 = numpy.array ([4, 3, 2])
+exp = sum ** array3
+print (exp)
+exp2 = sum **2
+print (exp2)
+someList = [22, 33, 55]
+array4 = numpy.array(someList)
+print (array4)
+x = input()
+
